7acab9ab399553ee5bceda34eae3408b4915f9cb
Sple <Shenb051102@outlook.com>
2026-04-20 19:39:42 +0800

    模拟

---
+++ ABC/454/A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+using i128 = __int128;
+#define lowbit(x) (x) & (-x)
+#define vec vector<int>
+#define vvec vector<vector<int>>
+#define rep(i, a, b) for(int i = (a); i <= (int)(b); ++i)
+const int INF = 1 << 30;
+const ll LLINF = 1LL << 60;
+const double eps = 1e-7;
+const int N =  2e5 + 10;
+void solve(){
+    int l, r;
+    cin >> l >> r;
+    cout << r - l + 1 << "\n";
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t = 1;
+    while (t--) solve();
+    return 0;
+}
